f93741e21b4779b0934d21f11dd4eabd74241449
Mwabisi <pmwabisiradebe@gmail.com>
2023-09-07 22:56:46 +0200

    Add file

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,8 +1,6 @@
-const add = (a, b) => {
-    return a + b;
-  };
-  
-  console.log("Correct output - 4 + 85: " + add(4, 85));
-  console.log("Correct output - 93 + -4: " + add(93, -4));
-  console.log("Correct output - 0 + 89: " + add(0, 89));
-  
+def add(a, b):
+  return a + b
+
+print(add(4, 85))
+print(add(93, -4))
+print(add(0, 89))
