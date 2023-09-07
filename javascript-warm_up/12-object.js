34195f293cb83debe105a668d41827690b3c1b1b
Mwabisi <pmwabisiradebe@gmail.com>
2023-09-07 22:21:28 +0200

    Object

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,6 +1,10 @@
 const numbers = [12, 34, 56, 78, 90];
 
-// Replace the value 12 with 89
-numbers[0] = 89;
+// Replace the value 12 with 89 using the array's forEach() method
+numbers.forEach((number, index) => {
+  if (number === 12) {
+    numbers[index] = 89;
+  }
+});
 
 console.log(numbers);
