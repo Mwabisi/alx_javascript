186fefa1b90672b98eeadb2dfd6d4e90b66e549a
Mwabisi <pmwabisiradebe@gmail.com>
2023-09-07 21:50:35 +0200

    3 languages

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+const cIsFun = "C is fun";
+const pythonIsCool = "Python is cool";
+const javascriptIsAmazing = "JavaScript is amazing";
+
+console.log(cIsFun);
+console.log(pythonIsCool);
+console.log(javascriptIsAmazing);
