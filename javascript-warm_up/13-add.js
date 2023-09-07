8cdf21411fab671734c5d374afa47ba4484c9d81
Mwabisi <pmwabisiradebe@gmail.com>
2023-09-07 22:32:22 +0200

    Add file

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,6 +1,5 @@
-const numbers = [12, 34, 56, 78, 90];
-
-// Replace the value 12 with 89
-numbers[0] = 89;
-
-console.log(numbers);
+const add = (a, b) => {
+    return a + b;
+  };
+  
+  export default add;  
