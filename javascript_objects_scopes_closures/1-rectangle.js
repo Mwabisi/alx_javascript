aa3cf8d10e04203f5c7d14fbc63fa7542168397f
Mwabisi <pmwabisiradebe@gmail.com>
2023-09-12 11:02:38 +0200

    Rectangle #1

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,6 +1,9 @@
 class Rectangle {
     constructor(w, h) {
-      this.width = w;
-      this.height = h;
+        this.width = w;
+        this.height = h;
     }
-  }
+}
+
+const rectangle = new Rectangle(3, 3);
+console.log(`Instance width: ${rectangle.width} - height: ${rectangle.height}`);
