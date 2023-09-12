63a34fb4b645b3dfb8069ea16d2d273712f96c75
Mwabisi <pmwabisiradebe@gmail.com>
2023-09-12 16:24:22 +0200

    Rectangle #1

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,26 +1,10 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
-      if (w > 0 && h > 0) {
-          this.width = w;
-          this.height = h;
-      } else {
-          this.width = undefined;
-          this.height = undefined;
-      }
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
   }
 }
-const rectangle1 = new Rectangle(3, 3);
-console.log(`Instance width: ${rectangle1.width} - height: ${rectangle1.height}`);
 
-const rectangle2 = new Rectangle(3, -3);
-console.log(`Instance width: ${rectangle2.width} - height: ${rectangle2.height}`);
-
-const rectangle3 = new Rectangle(3);
-console.log(`Instance width: ${rectangle3.width}`);
-
-const rectangle4 = new Rectangle(undefined, 3);
-console.log(`Instance width: ${rectangle4.width} - height: ${rectangle4.height}`);
-
-const rectangle5 = new Rectangle();
-console.log("Instance no argument");
+console.log(Rectangle);
+console.log(Rectangle);
