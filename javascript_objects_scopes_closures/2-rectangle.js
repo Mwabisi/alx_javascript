936b2543445eccf9516fcafa2f81eba2023c876f
Mwabisi <pmwabisiradebe@gmail.com>
2023-09-12 11:45:41 +0200

    Rectangle #2

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,15 @@
+class Rectangle {
+    constructor(w, h) {
+      if (!isPositiveInteger(w) || !isPositiveInteger(h)) {
+        this = null;
+      } else {
+        this.width = w;
+        this.height = h;
+      }
+    }
+  
+    static isPositiveInteger(n) {
+      return n >= 1 && Number.isInteger(n);
+    }
+  }
+  
