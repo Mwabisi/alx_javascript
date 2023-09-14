a44a277f3ad599c6f1fc426eedc01b82335da047
Mwabisi <pmwabisiradebe@gmail.com>
2023-09-14 20:35:07 +0200

    Rectangle #2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
 class Rectangle {
     constructor(width, height) {
-      if (width <= 0 || height <= 0) {
-        this = null;
-      } else {
-        this.width = width;
-        this.height = height;
+      if (w < 0 && h > 0) {
+        this.width = w;
+        this.height = h;
       }
     }
   }
-  
