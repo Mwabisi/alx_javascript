d09232f1e64b5f0a5297c81cf2de1bfa974aa7b8
Mwabisi <pmwabisiradebe@gmail.com>
2023-09-15 17:33:14 +0200

    Rectangle #1

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,7 +1,10 @@
-
-module.export = class Rectangle {
-  constructor(w, h){
-    this.width = w;
-    this.height = h;
-}
+module.exports = class Rectangle {
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      console.error("Invalid arguments. Both width and height must be positive numbers.");
+    }
+  }
 };
