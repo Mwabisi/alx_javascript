b8e33ab1218201ffb6ebfd4112345295702078dc
Mwabisi <pmwabisiradebe@gmail.com>
2023-09-15 17:42:15 +0200

    Rectangle #1 updated error

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,10 +1,9 @@
-module.exports = class Rectangle {
+#!/usr/bin/node
+class Rectangle {
   constructor(w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
-    } else {
-      console.error("Invalid arguments. Both width and height must be positive numbers.");
     }
   }
 };
