9c4957374346e4d971f36584c8be21bbc710df90
Mwabisi <pmwabisiradebe@gmail.com>
2023-09-19 11:22:14 +0200

    Square #0

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,48 +1,28 @@
-class Square {
-    constructor(size) {
-      if (size < 0) {
-        throw new Error('Invalid square size.');
-      }
-  
-      this.size = size;
+class Rectangle {
+    constructor(width, height) {
+      this.width = width;
+      this.height = height;
     }
   
-    print() {
-      for (let i = 0; i < this.size; i++) {
-        for (let j = 0; j < this.size; j++) {
-          console.log('*');
-        }
-  
-        console.log('\n');
-      }
-    }
-  
-    double() {
-      return new Square(this.size * 2);
-    }
-  
-    rotate() {
-      return new Square(this.size).map((row, i) => {
-        return row.map((cell, j) => {
-          return this.get(j, i);
-        });
-      });
-    }
-  
-    get(row, column) {
-      if (row < 0 || row >= this.size || column < 0 || column >= this.size) {
-        return undefined;
-      }
-  
-      return this.cells[row][column];
-    }
-  
-    set(row, column, value) {
-      if (row < 0 || row >= this.size || column < 0 || column >= this.size) {
-        return;
-      }
-  
-      this.cells[row][column] = value;
+    area() {
+      return this.width * this.height;
     }
   }
   
+  class Square extends Rectangle {
+    constructor(size) {
+      super(size, size);
+    }
+  }
+
+  const square = new Square(4);
+
+  square.print(); // Prints the square
+
+const doubleSquare = square.double();
+
+doubleSquare.print(); 
+
+const rotatedSquare = square.rotate(); 
+
+rotatedSquare.print(); 
