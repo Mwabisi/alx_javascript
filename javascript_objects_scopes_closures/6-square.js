16d36bd31e1972a155381f27277cc5afa6ed7a4a
Mwabisi <pmwabisiradebe@gmail.com>
2023-09-19 13:11:37 +0200

    Square #1

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,19 +1,16 @@
-const square = new Square(5);
-
-square.charPrint('D'); // Prints the square using the character 'D'
-
-square.charPrint(); // Prints the square using the character 'X'
-
-square.charPrint('KKK'); // Prints the square using the character 'KKK'
-
-// Create a new square object with size 3
-const smallSquare = new Square(3);
-
-// Print the small square using the character 'D'
-smallSquare.charPrint('D');
-
-// Double the size of the small square
-smallSquare.double();
-
-// Print the doubled small square using the character 'O'
-smallSquare.charPrint('O');
+class Square extends Square {
+    constructor(size) {
+      super(size);
+    }
+  
+    charPrint(c = 'X') {
+      for (let i = 0; i < this.size; i++) {
+        for (let j = 0; j < this.size; j++) {
+          console.log(c);
+        }
+  
+        console.log('\n');
+      }
+    }
+  }
+  
