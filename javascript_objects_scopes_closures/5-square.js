2c8fe16de0346e4a1fe5bd951efbaa8e4bffdc07
Mwabisi <pmwabisiradebe@gmail.com>
2023-09-19 11:39:45 +0200

    Square #0

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,28 +1,51 @@
-class Rectangle {
-    constructor(width, height) {
-      this.width = width;
-      this.height = height;
-    }
-  
-    area() {
-      return this.width * this.height;
-    }
-  }
-  
-  class Square extends Rectangle {
+class Square {
     constructor(size) {
-      super(size, size);
+      if (size < 0) {
+        throw new Error('Invalid square size.');
+      }
+  
+      this.size = size;
+    }
+  
+    print() {
+      for (let i = 0; i < this.size; i++) {
+        for (let j = 0; j < this.size; j++) {
+          console.log('*');
+        }
+  
+        console.log('\n');
+      }
+    }
+  
+    double() {
+      return new Square(this.size * 2);
+    }
+  
+    rotate() {
+      return new Square(this.size).map((row, i) => {
+        return row.map((cell, j) => {
+          return this.get(j, i);
+        });
+      });
+    }
+  
+    get(row, column) {
+      if (row < 0 || row >= this.size || column < 0 || column >= this.size) {
+        return undefined;
+      }
+  
+      return this.cells[row][column];
+    }
+  
+    set(row, column, value) {
+      if (row < 0 || row >= this.size || column < 0 || column >= this.size) {
+        return;
+      }
+  
+      this.cells[row][column] = value;
     }
   }
 
-  const square = new Square(4);
+  const square = new Square(-4);
 
-  square.print(); // Prints the square
-
-const doubleSquare = square.double();
-
-doubleSquare.print(); 
-
-const rotatedSquare = square.rotate(); 
-
-rotatedSquare.print(); 
+// Error: Invalid square size.
