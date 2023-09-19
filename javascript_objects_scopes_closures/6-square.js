0d9f156ac3e5b043e9e9beef4afc4de5fc424634
Mwabisi <pmwabisiradebe@gmail.com>
2023-09-19 12:57:14 +0200

    Square #1

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+class Square extends Square {
+    constructor(size) {
+      super(size);
+    }
+  
+    charPrint(c = 'X') {
+      for (let i = 0; i < this.size; i++) {
+        for (let j = 0; j < this.size; j++) {
+          console.log(c);
+        }
+  
+        console.log('\n');
+      }
+    }
+  }
+
+  const square = new Square(4);
+
+square.charPrint();
+
+square.charPrint('#');
