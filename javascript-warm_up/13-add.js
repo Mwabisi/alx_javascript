2dd5f9be071ddae7724159cc803bcdcdf82f9ef1
Mwabisi <pmwabisiradebe@gmail.com>
2023-09-21 13:16:16 +0200

    Add file

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,14 +1,4 @@
-function add(a, b) {
-    /**
-     * Returns the addition of 2 integers.
-     *
-     * @param {number} a The first integer.
-     * @param {number} b The second integer.
-     * @returns {number} The sum of a and b.
-     */
-    return a + b;
-  }
-  
-  console.log(add(4, 85));
-  console.log(add(93, -4));
-  console.log(add(0, 8));  
+#!/usr/bin/node
+#!/usr/bin/node
+const add = require('./13-add').add;
+console.log(add(3, 5));
