532a27499662419380898a9e5d0eb03d7d93a875
Mwabisi <pmwabisiradebe@gmail.com>
2023-09-22 22:31:33 +0200

    0. Status code

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
-const getStatusCode = async (url) => {
+async function getStatusCode(url) {
     const response = await fetch(url);
     return response.status;
-  };
+  }
   
-  const main = async () => {
+  async function main() {
     const url = prompt('Enter the URL to request: ');
     const statusCode = await getStatusCode(url);
   
     console.log(`code: ${statusCode}`);
-  };
+  }
   
-  main();
+  main();  
