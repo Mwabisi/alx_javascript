ec6c30c0effcc3c35d9d97ccef8b1906f4e9573d
Mwabisi <pmwabisiradebe@gmail.com>
2023-09-22 22:36:37 +0200

    0. Status code

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,14 +1,30 @@
 #!/usr/bin/node
-async function getStatusCode(url) {
-    const response = await fetch(url);
-    return response.status;
-  }
-  
-  async function main() {
-    const url = prompt('Enter the URL to request: ');
-    const statusCode = await getStatusCode(url);
-  
-    console.log(`code: ${statusCode}`);
-  }
-  
-  main();  
+const http = require('http');
+
+// Function to get and display the status code and message
+function getStatus(url) {
+    const req = http.get(url, (res) => {
+        console.log(`Status Code: ${res.statusCode}`);
+        if (res.statusCode === 200) {
+            console.log(`Path exists.`);
+        } else if (res.statusCode === 404) {
+            console.log(`Path doesn't exist.`);
+        } else if (res.statusCode === 301) {
+            console.log(`Path redirects.`);
+        } else if (res.statusCode === 204) {
+            console.log(`Path has no content.`);
+        } else {
+            console.log(`Unexpected status code: ${res.statusCode}`);
+        }
+    });
+
+    req.on('error', (error) => {
+        console.error(`Request failed: ${error.message}`);
+    });
+}
+
+// Input URL from the user (replace 'https://example.com' with your desired URL)
+const url = 'https://example.com';
+
+// Call the function with the provided URL
+getStatus(url);
