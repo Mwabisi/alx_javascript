13d46a124b70771dbdbc4ce8307d662709109103
Mwabisi <pmwabisiradebe@gmail.com>
2023-09-23 12:47:52 +0200

    Rectangle #2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,25 @@
 #!/usr/bin/node
-const Rectangle = require('./Rectangle');
+class Rectangle {
   constructor(w, h) {
-    if (w > 0 && h > 0) {
+    if (w <= 0 || h <= 0) {
+      this.width = undefined;
+      this.height = undefined;
+    } else {
       this.width = w;
       this.height = h;
-    } else {
-      console.error("Invalid arguments. Both width and height must be positive numbers.");
     }
   }
+}
+
+// Creating instances of the Rectangle class
+let rectangle1 = new Rectangle(2, 3);
+console.log(rectangle1.width);  
+console.log(rectangle1.height);  
+
+let rectangle2 = new Rectangle(0, 4);
+console.log(rectangle2.width);  
+console.log(rectangle2.height); 
+
+let rectangle3 = new Rectangle(-1, -1);
+console.log(rectangle3.width);  
+console.log(rectangle3.height);
