21c813f171a7ae23058915e451094b23723c1581
Mwabisi <pmwabisiradebe@gmail.com>
2023-09-23 13:09:00 +0200

    Square #0

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,42 +1,50 @@
-#!/usr/bin/node
-
 module.exports = class Rectangle {
-    constructor(w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
-    }
-  
-    print() {
-      if (this.height && this.width) {
-        const row = 'X'.repeat(this.width);
-        for (let i = 0; i < this.height; i++) {
-          console.log(row);
-        }
-      }
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
   }
-  
-    rotate() {
-      [this.width, this.height] = [this.height, this.width];
+
+  print() {
+    if (this.height && this.width) {
+      const row = 'X'.repeat(this.width);
+      for (let i = 0; i < this.height; i++) {
+        console.log(row);
+      }
+    } else {
+      console.error('The rectangle is empty.');
     }
-  
-    double() {
-      this.width *= 2;
-      this.height *= 2;
-    }
-}
+  }
+
+  rotate() {
+    [this.width, this.height] = [this.height, this.width];
+    return !!this.width && !!this.height;
+  }
+
+  double() {
+    this.width *= 2;
+    this.height *= 2;
+    return !!this.width && !!this.height;
+  }
+};
 
 class Square extends Rectangle {
-    constructor(size) {
-        super(size, size);
-    }
+  constructor(size) {
+    super(size, size);
+  }
 }
 
 const mySquare = new Square(5);
+
 mySquare.print();
-mySquare.rotate();
-mySquare.print();
-mySquare.double();
-mySquare.print();
+
+const rotated = mySquare.rotate();
+if (rotated) {
+  mySquare.print(); 
+}
+const doubled = mySquare.double(); 
+
+if (doubled) {
+  mySquare.print(); 
+}
