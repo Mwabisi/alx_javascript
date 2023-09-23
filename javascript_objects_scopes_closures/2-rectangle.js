549eb5cc59ba8de1a18b605d5922f7c42feeb06a
Mwabisi <pmwabisiradebe@gmail.com>
2023-09-23 12:54:01 +0200

    Rectangle #2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -12,14 +12,22 @@ class Rectangle {
 }
 
 // Creating instances of the Rectangle class
-let rectangle1 = new Rectangle(2, 3);
-console.log(rectangle1.width);  
+let rectangle1 = new Rectangle(3, 3);
+console.log(rectangle1.width); 
 console.log(rectangle1.height);  
 
-let rectangle2 = new Rectangle(0, 4);
+let rectangle2 = new Rectangle(3, -3);
 console.log(rectangle2.width);  
-console.log(rectangle2.height); 
+console.log(rectangle2.height);  
 
-let rectangle3 = new Rectangle(-1, -1);
+let rectangle3 = new Rectangle(3);
 console.log(rectangle3.width);  
-console.log(rectangle3.height);
+console.log(rectangle3.height);  
+
+let rectangle4 = new Rectangle(undefined, 3);
+console.log(rectangle4.width);  
+console.log(rectangle4.height);  
+
+let rectangle5 = new Rectangle();
+console.log(rectangle5.width);  
+console.log(rectangle5.height);  
