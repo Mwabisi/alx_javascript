b189488aad92b4b9b745e9613f7035d313e3770f
Mwabisi <pmwabisiradebe@gmail.com>
2023-09-24 22:52:07 +0200

    Closure Scope Chain

---
+++ Javascript_advanced/1-nested_functions.js
@@ -0,0 +1,16 @@
+function welcome() {
+    alert("Welcome");
+  }
+  
+  function welcomeHolberton() {
+    alert("Welcome Holberton");
+  }
+  
+  function welcomeHolbertonExclamation() {
+    alert("Welcome Holberton!");
+  }
+  
+  // Call the functions one by one.
+  welcome();
+  welcomeHolberton();
+  welcomeHolbertonExclamation();  
