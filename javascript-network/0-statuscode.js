416155eeeea8f0157742184c134afc828600cde7
Mwabisi <pmwabisiradebe@gmail.com>
2023-09-24 20:25:12 +0200

    Status code

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,30 +1,15 @@
 #!/usr/bin/node
-const http = require('http');
+const request = require('request');
 
-// Function to get and display the status code and message
-function getStatus(url) {
-    const req = http.get(url, (res) => {
-        console.log(`Status Code: ${res.statusCode}`);
-        if (res.statusCode === 200) {
-            console.log(`Path exists.`);
-        } else if (res.statusCode === 404) {
-            console.log(`Path doesn't exist.`);
-        } else if (res.statusCode === 301) {
-            console.log(`Path redirects.`);
-        } else if (res.statusCode === 204) {
-            console.log(`Path has no content.`);
-        } else {
-            console.log(`Unexpected status code: ${res.statusCode}`);
-        }
-    });
+// Define the URL as the first command-line argument
+const url = process.argv[2];
 
-    req.on('error', (error) => {
-        console.error(`Request failed: ${error.message}`);
-    });
-}
-
-// Input URL from the user (replace 'https://example.com' with your desired URL)
-const url = 'https://example.com';
-
-// Call the function with the provided URL
-getStatus(url);
+// Make a GET request to the URL
+request.get(url, (error, response) => {
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    // Display the status code of the response
+    console.log(`code: ${response.statusCode}`);
+  }
+});
