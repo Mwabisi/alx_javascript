46824c60f58c0e8261d55626f7e61c3828395c55
Mwabisi <pmwabisiradebe@gmail.com>
2023-09-24 20:12:20 +0200

    Loripsum

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -2,21 +2,23 @@
 const request = require('request');
 const fs = require('fs');
 
-// Define the URL as the first command-line argument
-const url = process.argv[2];
+const downloadWebpage = async (url, filePath) => {
+  const response = await request(url);
 
-// Define the file path as the second command-line argument
-const filePath = process.argv[3];
-
-// Make a GET request to the URL
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-  } else if (response.statusCode !== 200) {
-    console.error('Request failed with status code:', response.statusCode);
-  } else {
-    // Write the response body to the specified file with UTF-8 encoding
-    fs.writeFileSync(filePath, body, { encoding: 'utf-8' });
-    console.log(`Webpage contents successfully saved to ${filePath}`);
+  // Check if the response was successful.
+  if (response.statusCode !== 200) {
+    throw new Error(`Failed to download webpage: ${url}`);
   }
-});
+
+  // Save the response body to the file.
+  fs.writeFileSync(filePath, response.body, { encoding: 'utf-8' });
+
+  console.log(`Webpage downloaded to ${filePath}`);
+};
+
+// Example usage:
+
+const url = 'https://www.google.com';
+const filePath = './google.html';
+
+downloadWebpage(url, filePath);
