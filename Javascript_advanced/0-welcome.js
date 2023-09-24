75106c65e2d476c7f2d3ee0f5e464c26dd7b4392
Mwabisi <pmwabisiradebe@gmail.com>
2023-09-24 22:31:59 +0200

    Lexical scoping and welcome message

--- Javascript_advanced/0-welcome.js
+++ Javascript_advanced/0-welcome.js
@@ -1,10 +1,17 @@
 #!/usr/bin/node
 function welcome(firstName, lastName) {
-    const fullName = `${firstName} ${lastName}`;
+    const fullName = firstName + ' ' + lastName;
   
     function displayFullName() {
       alert(`Welcome ${fullName}!`);
     }
   
     displayFullName();
-  }  
+  
+    return fullName; 
+  }
+  
+  const fullNameReturned = welcome('Holberton', 'School'); 
+  
+  console.log(fullNameReturned); 
+  
